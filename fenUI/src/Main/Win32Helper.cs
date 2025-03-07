7b99d4cbba30a63021eedf1856d9d42ade3c5d9c
fen4flo <fb8599422@gmail.com>
2025-03-07 17:41:19 +0100

    Add Win32Helper

--- fenUI/src/Main/Win32Helper.cs
+++ fenUI/src/Main/Win32Helper.cs
@@ -1,259 +1,268 @@
-namespace FenUISharp {
-using System;
-using System.Runtime.InteropServices;
-
-public static class Win32Helper
+namespace FenUISharp
 {
-    #region Enums & Constants
+    using System;
+    using System.Runtime.InteropServices;
 
-    // Window Messages
-    public enum WindowMessages : uint
+    public static class Win32Helper
     {
-        WM_DESTROY    = 0x0002,
-        WM_PAINT      = 0x000F,
-        WM_SIZE       = 0x0005,
-        WM_KEYDOWN    = 0x0100,
-        WM_MOUSEMOVE  = 0x0200,
-        WM_LBUTTONDOWN= 0x0201,
-        WM_DROPFILES  = 0x0233,
-        WM_SETCURSOR  = 0x0020,
-        WM_TIMER      = 0x0113,
-        WM_QUIT       = 0x0012
+        #region Enums & Constants
+
+        // Window Messages
+        public enum WindowMessages : uint
+        {
+            WM_DESTROY = 0x0002,
+            WM_PAINT = 0x000F,
+            WM_SIZE = 0x0005,
+            WM_KEYDOWN = 0x0100,
+            WM_MOUSEMOVE = 0x0200,
+            WM_LBUTTONDOWN = 0x0201,
+            WM_DROPFILES = 0x0233,
+            WM_SETCURSOR = 0x0020,
+            WM_TIMER = 0x0113,
+            WM_QUIT = 0x0012
+        }
+
+        // ShowWindow commands
+        public enum ShowWindowCommands : int
+        {
+            SW_SHOWNORMAL = 1
+        }
+
+        // Window longs
+        public enum WindowLongs : int {
+            GWL_EXSTYLE = -20
+        }
+
+        // Window styles
+        public enum WindowStyles : int
+        {
+            WS_POPUP = unchecked((int)0x80000000),
+            WS_EX_LAYERED = 0x00080000,
+            WS_EX_APPWINDOW = 0x00040000,
+            WS_EX_TOOLWINDOW = 0x00000080
+        }
+
+        // SetWindowPos flags
+        [Flags]
+        public enum SetWindowPosFlags : uint
+        {
+            SWP_NOMOVE = 0x0002,
+            SWP_NOSIZE = 0x0001,
+            SWP_SHOWWINDOW = 0x0040,
+        }
+
+        // PeekMessage options
+        public enum PeekMessageRemoveOptions : uint
+        {
+            PM_REMOVE = 0x0001
+        }
+
+        // Layered Window flags
+        public enum LayeredWindowFlags : uint
+        {
+            ULW_ALPHA = 0x00000002
+        }
+
+        // Alpha blend options used in BLENDFUNCTION
+        public enum AlphaBlendOptions : byte
+        {
+            AC_SRC_OVER = 0x00,
+            AC_SRC_ALPHA = 0x01
+        }
+
+        public const int CW_USEDEFAULT = unchecked((int)0x80000000);
+        public const int IDC_ARROW = 32512;
+        public const int HWND_TOPMOST = -1;
+        public const int HWND_NOTOPMOST = -2;
+
+        #endregion
+
+        #region Structs
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct WNDCLASSEX
+        {
+            public uint cbSize;
+            public uint style;
+            public WndProcDelegate lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpszMenuName;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpszClassName;
+            public IntPtr hIconSm;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
+        {
+            public IntPtr hwnd;
+            public uint message;
+            public UIntPtr wParam;
+            public IntPtr lParam;
+            public uint time;
+            public POINT pt;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SIZE
+        {
+            public int cx;
+            public int cy;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct BLENDFUNCTION
+        {
+            public byte BlendOp;
+            public byte BlendFlags;
+            public byte SourceConstantAlpha;
+            public byte AlphaFormat;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BITMAPINFOHEADER
+        {
+            public uint biSize;
+            public int biWidth;
+            public int biHeight;
+            public ushort biPlanes;
+            public ushort biBitCount;
+            public uint biCompression;
+            public uint biSizeImage;
+            public int biXPelsPerMeter;
+            public int biYPelsPerMeter;
+            public uint biClrUsed;
+            public uint biClrImportant;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BITMAPINFO
+        {
+            public BITMAPINFOHEADER bmiHeader;
+            public uint bmiColors; // Not used for 32-bit DIBs
+        }
+
+        #endregion
+
+        #region Delegate
+
+        // Delegate for window procedure callbacks
+        public delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        #endregion
+
+        #region Win32 API Functions
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern ushort RegisterClassEx(ref WNDCLASSEX lpwcx);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr CreateWindowEx(
+             int dwExStyle,
+             string lpClassName,
+             string lpWindowName,
+             int dwStyle,
+             int x,
+             int y,
+             int nWidth,
+             int nHeight,
+             IntPtr hWndParent,
+             IntPtr hMenu,
+             IntPtr hInstance,
+             IntPtr lpParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool UpdateWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DefWindowProcW(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern void PostQuitMessage(int nExitCode);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+        [DllImport("user32.dll")]
+        public static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+        [DllImport("user32.dll")]
+        public static extern bool PeekMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax, uint wRemoveMsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetCursor(IntPtr hCursor);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UpdateLayeredWindow(IntPtr hwnd, IntPtr hdcDst,
+             ref POINT pptDst, ref SIZE psize, IntPtr hdcSrc,
+             ref POINT pptSrc, uint crKey, ref BLENDFUNCTION pblend, uint dwFlags);
+
+        [DllImport("shell32.dll")]
+        public static extern void DragAcceptFiles(IntPtr hWnd, bool accept);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern IntPtr CreateDIBSection(IntPtr hdc, [In] ref BITMAPINFO pbmi,
+             uint iUsage, out IntPtr ppvBits, IntPtr hSection, uint dwOffset);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern bool DeleteDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("dwmapi.dll")]
+        public static extern void DwmFlush();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern UIntPtr SetTimer(IntPtr hWnd, UIntPtr nIDEvent, uint uElapse, IntPtr lpTimerFunc);
+
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+  
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        #endregion
     }
-
-    // ShowWindow commands
-    public enum ShowWindowCommands : int
-    {
-        SW_SHOWNORMAL = 1
-    }
-
-    // Window styles
-    public enum WindowStyles : int
-    {
-        WS_POPUP = unchecked((int)0x80000000)
-    }
-
-    // Extended Window styles
-    public enum ExtendedWindowStyles : int
-    {
-        WS_EX_LAYERED = 0x00080000
-    }
-
-    // SetWindowPos flags
-    [Flags]
-    public enum SetWindowPosFlags : uint
-    {
-        SWP_NOMOVE    = 0x0002,
-        SWP_NOSIZE    = 0x0001,
-        SWP_SHOWWINDOW= 0x0040,
-    }
-
-    // PeekMessage options
-    public enum PeekMessageRemoveOptions : uint
-    {
-        PM_REMOVE = 0x0001
-    }
-
-    // Layered Window flags
-    public enum LayeredWindowFlags : uint
-    {
-        ULW_ALPHA = 0x00000002
-    }
-
-    // Alpha blend options used in BLENDFUNCTION
-    public enum AlphaBlendOptions : byte
-    {
-        AC_SRC_OVER  = 0x00,
-        AC_SRC_ALPHA = 0x01
-    }
-
-    public const int CW_USEDEFAULT = unchecked((int)0x80000000);
-    public const int IDC_ARROW = 32512;
-    public const int HWND_TOPMOST = -1;
-    public const int HWND_NOTOPMOST = -2;
-
-    #endregion
-
-    #region Structs
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-    public struct WNDCLASSEX
-    {
-        public uint cbSize;
-        public uint style;
-        public WndProcDelegate lpfnWndProc;
-        public int cbClsExtra;
-        public int cbWndExtra;
-        public IntPtr hInstance;
-        public IntPtr hIcon;
-        public IntPtr hCursor;
-        public IntPtr hbrBackground;
-        [MarshalAs(UnmanagedType.LPTStr)]
-        public string lpszMenuName;
-        [MarshalAs(UnmanagedType.LPTStr)]
-        public string lpszClassName;
-        public IntPtr hIconSm;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MSG
-    {
-        public IntPtr hwnd;
-        public uint message;
-        public UIntPtr wParam;
-        public IntPtr lParam;
-        public uint time;
-        public POINT pt;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct POINT
-    {
-        public int x;
-        public int y;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct SIZE
-    {
-        public int cx;
-        public int cy;
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct BLENDFUNCTION
-    {
-        public byte BlendOp;
-        public byte BlendFlags;
-        public byte SourceConstantAlpha;
-        public byte AlphaFormat;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct BITMAPINFOHEADER
-    {
-        public uint biSize;
-        public int biWidth;
-        public int biHeight;
-        public ushort biPlanes;
-        public ushort biBitCount;
-        public uint biCompression;
-        public uint biSizeImage;
-        public int biXPelsPerMeter;
-        public int biYPelsPerMeter;
-        public uint biClrUsed;
-        public uint biClrImportant;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct BITMAPINFO
-    {
-        public BITMAPINFOHEADER bmiHeader;
-        public uint bmiColors; // Not used for 32-bit DIBs
-    }
-
-    #endregion
-
-    #region Delegate
-
-    // Delegate for window procedure callbacks
-    public delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-
-    #endregion
-
-    #region Win32 API Functions
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    public static extern ushort RegisterClassEx(ref WNDCLASSEX lpwcx);
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    public static extern IntPtr CreateWindowEx(
-         int dwExStyle,
-         string lpClassName,
-         string lpWindowName,
-         int dwStyle,
-         int x,
-         int y,
-         int nWidth,
-         int nHeight,
-         IntPtr hWndParent,
-         IntPtr hMenu,
-         IntPtr hInstance,
-         IntPtr lpParam);
-
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    [DllImport("user32.dll")]
-    public static extern bool UpdateWindow(IntPtr hWnd);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr DefWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-
-    [DllImport("user32.dll")]
-    public static extern void PostQuitMessage(int nExitCode);
-
-    [DllImport("user32.dll")]
-    public static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
-
-    [DllImport("user32.dll")]
-    public static extern bool TranslateMessage([In] ref MSG lpMsg);
-
-    [DllImport("user32.dll")]
-    public static extern bool PeekMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax, uint wRemoveMsg);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr SetCursor(IntPtr hCursor);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr GetDC(IntPtr hWnd);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool UpdateLayeredWindow(IntPtr hwnd, IntPtr hdcDst,
-         ref POINT pptDst, ref SIZE psize, IntPtr hdcSrc,
-         ref POINT pptSrc, uint crKey, ref BLENDFUNCTION pblend, uint dwFlags);
-
-    [DllImport("shell32.dll")]
-    public static extern void DragAcceptFiles(IntPtr hWnd, bool accept);
-
-    [DllImport("gdi32.dll", SetLastError = true)]
-    public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
-
-    [DllImport("gdi32.dll", SetLastError = true)]
-    public static extern IntPtr CreateDIBSection(IntPtr hdc, [In] ref BITMAPINFO pbmi,
-         uint iUsage, out IntPtr ppvBits, IntPtr hSection, uint dwOffset);
-
-    [DllImport("gdi32.dll", SetLastError = true)]
-    public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
-
-    [DllImport("gdi32.dll", SetLastError = true)]
-    public static extern bool DeleteDC(IntPtr hdc);
-
-    [DllImport("gdi32.dll", SetLastError = true)]
-    public static extern bool DeleteObject(IntPtr hObject);
-
-    [DllImport("dwmapi.dll")]
-    public static extern void DwmFlush();
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern UIntPtr SetTimer(IntPtr hWnd, UIntPtr nIDEvent, uint uElapse, IntPtr lpTimerFunc);
-
-    [DllImport("user32.dll")]
-    public static extern int GetSystemMetrics(int nIndex);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-
-    #endregion
-}
 
 }
--- fenUI/src/Window/FWindow.cs
+++ fenUI/src/Window/FWindow.cs
@@ -1,43 +1,291 @@
-namespace FenUISharp {
-    public class FWindow {
-        
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using FenUISharpTest1;
+using SkiaSharp;
+
+namespace FenUISharp
+{
+    public class FWindow
+    {
+
+        // Window Specifications
+
+        public double WindowRefreshRate { get; set; } = 5.0;
+
         public string WindowTitle { get; private set; }
         public string WindowClass { get; private set; }
-        
-        public IntPtr hWnd { get; private set; }
 
-        public FWindow(string windowTitle, string windowClass) {
-            CreateWin32Window();
+        public int WindowWidth { get; private set; }
+        public int WindowHeight { get; private set; }
+
+        public IntPtr hWnd { get; private set; } // Window Handle
+
+        // Private variables
+        private static IntPtr _hdcMemory = IntPtr.Zero; // Memory DC
+        private static IntPtr _hBitmap = IntPtr.Zero;     // Handle to our DIB section
+        private static IntPtr _ppvBits = IntPtr.Zero;     // Pointer to pixel bits
+
+        // Rendering / SkiaSharp
+        private static SKSurface _surface;
+        private static SKCanvas _canvas;
+
+        public FWindow(string windowTitle, string windowClass)
+        {
+            WindowTitle = windowTitle;
+            WindowClass = windowClass;
+
+            hWnd = CreateWin32Window();
+            Win32Helper.DragAcceptFiles(hWnd, true);
+
+            SetAlwaysOnTop();
+            RemoveTaskbarIcon();
         }
 
-        private IntPtr CreateWin32Window(){
-            string className = WindowClass;
-            WNDCLASSEX wndClass = new WNDCLASSEX
+        public void Begin()
+        {
+            // Begin render stopwatch
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            double lastFrameTime = 0;
+            double frameTime = 1000.0 / WindowRefreshRate;
+
+            // Message loop
+            Win32Helper.MSG msg;
+            while (true)
             {
-                cbSize = (uint)Marshal.SizeOf(typeof(WNDCLASSEX)),
+                while (Win32Helper.PeekMessage(out msg, IntPtr.Zero, 0, 0, (int)Win32Helper.PeekMessageRemoveOptions.PM_REMOVE))
+                {
+                    if (msg.message == (int)Win32Helper.WindowMessages.WM_QUIT)
+                        return; // Exit loop properly
+
+                    Win32Helper.TranslateMessage(ref msg);
+                    Win32Helper.DispatchMessage(ref msg);
+                }
+
+                // Frame timing
+                double elapsed = stopwatch.Elapsed.TotalMilliseconds;
+                if (elapsed - lastFrameTime >= frameTime)
+                {
+                    lastFrameTime = elapsed;
+                    Render(hWnd);
+                }
+
+                // Sleep a bit to avoid too much CPU usage
+                Thread.Sleep(1);
+            }
+        }
+
+        private void Render(IntPtr hWnd)
+        {
+            if (_surface == null)
+                return;
+
+            _canvas = _surface.Canvas;
+            // Clear the surface with fully transparent pixels.
+            _canvas.Clear(new SKColor(0, 0, 0, 0));
+
+            // Draw stuff
+            var rect = SKRect.Create(0, 0, 100, 100);
+
+string skslCode = @"
+        uniform vec2 u_resolution;
+        uniform vec4 u_color;
+        
+        half4 main(float2 fragCoord) {
+            vec2 p = (fragCoord / u_resolution) * 2.0 - 1.0; // Normalize -1 to 1
+            float r = 1.0;
+            float n = 3; // Squircle roundness
+
+            float d = pow(abs(p.x), n) + pow(abs(p.y), n);
+            if (d > pow(r, n)) {
+                return half4(0.0); // Transparent outside
+            }
+            
+            return u_color; // Inside squircle
+        }
+    ";
+
+    // Compile the shader
+    var effect = SKRuntimeEffect.CreateShader(skslCode, out var error);
+    if (effect == null) throw new Exception("Shader compile error: " + error);
+
+    // Set uniform values
+    var uniforms = new SKRuntimeEffectUniforms(effect);
+    uniforms["u_resolution"] = new SKPoint(rect.Width, rect.Height);
+    uniforms["u_color"] = SKColors.White;
+
+    // Create the shader
+    var shader = effect.ToShader(uniforms);
+
+    // Paint object with the shader
+    using var paint = new SKPaint { Shader = shader };
+    paint.IsAntialias = true;
+
+    // Draw on canvas
+    _canvas.DrawRect(rect, paint);
+
+            _canvas.Flush();
+
+            // Set destination point to (0,0) to eliminate margin
+            Win32Helper.POINT ptSrc = new Win32Helper.POINT { x = 0, y = 0 };
+            Win32Helper.POINT ptDst = new Win32Helper.POINT { x = 0, y = 0 }; // Fix: Position at top-left corner
+            Win32Helper.SIZE size = new Win32Helper.SIZE { cx = WindowWidth, cy = WindowHeight };
+
+            Win32Helper.BLENDFUNCTION blend = new Win32Helper.BLENDFUNCTION
+            {
+                BlendOp = (int)Win32Helper.AlphaBlendOptions.AC_SRC_OVER,
+                SourceConstantAlpha = 255,
+                AlphaFormat = (int)Win32Helper.AlphaBlendOptions.AC_SRC_ALPHA
+            };
+
+            IntPtr hdcScreen = Win32Helper.GetDC(IntPtr.Zero);
+            Win32Helper.UpdateLayeredWindow(
+                hWnd,
+                hdcScreen,
+                ref ptDst,  // Now (0,0)
+                ref size,
+                _hdcMemory,
+                ref ptSrc,
+                0,
+                ref blend,
+                (int)Win32Helper.LayeredWindowFlags.ULW_ALPHA
+            );
+            Win32Helper.ReleaseDC(IntPtr.Zero, hdcScreen);
+
+            Win32Helper.DwmFlush();
+        }
+
+        public void Cleanup()
+        {
+            if (_hdcMemory != IntPtr.Zero)
+                Win32Helper.DeleteDC(_hdcMemory);
+            if (_hBitmap != IntPtr.Zero)
+                Win32Helper.DeleteObject(_hBitmap);
+        }
+
+        public void CreateSurface()
+        {
+            SetupLayeredDC(hWnd);
+        }
+
+        // Set up a memory DC and a 32-bit DIB section for per-pixel alpha rendering.
+        private void SetupLayeredDC(IntPtr hWnd)
+        {
+            IntPtr hdcScreen = Win32Helper.GetDC(IntPtr.Zero);
+            _hdcMemory = Win32Helper.CreateCompatibleDC(hdcScreen);
+
+            Win32Helper.BITMAPINFO bmi = new Win32Helper.BITMAPINFO();
+            bmi.bmiHeader.biSize = (uint)Marshal.SizeOf(typeof(Win32Helper.BITMAPINFOHEADER));
+            bmi.bmiHeader.biWidth = WindowWidth;
+            bmi.bmiHeader.biHeight = -WindowHeight; // negative to create a top-down DIB
+            bmi.bmiHeader.biPlanes = 1;
+            bmi.bmiHeader.biBitCount = 32;
+            bmi.bmiHeader.biCompression = 0; // BI_RGB
+            bmi.bmiHeader.biSizeImage = (uint)(WindowWidth * WindowHeight * 4);
+            bmi.bmiHeader.biXPelsPerMeter = 0;
+            bmi.bmiHeader.biYPelsPerMeter = 0;
+            bmi.bmiHeader.biClrUsed = 0;
+            bmi.bmiHeader.biClrImportant = 0;
+
+            _hBitmap = Win32Helper.CreateDIBSection(hdcScreen, ref bmi, 0, out _ppvBits, IntPtr.Zero, 0);
+            Win32Helper.SelectObject(_hdcMemory, _hBitmap);
+
+            Win32Helper.ReleaseDC(IntPtr.Zero, hdcScreen);
+
+            // Create a SkiaSharp surface backed by the DIB's pixel memory.
+            var imageInfo = new SKImageInfo(WindowWidth, WindowHeight, SKColorType.Bgra8888, SKAlphaType.Premul);
+            _surface = SKSurface.Create(imageInfo, _ppvBits, imageInfo.RowBytes);
+        }
+
+        public void Show()
+        {
+            Win32Helper.ShowWindow(hWnd, (int)Win32Helper.ShowWindowCommands.SW_SHOWNORMAL);
+        }
+
+        private IntPtr CreateWin32Window()
+        {
+            string className = WindowClass;
+            Win32Helper.WNDCLASSEX wndClass = new Win32Helper.WNDCLASSEX
+            {
+                cbSize = (uint)Marshal.SizeOf(typeof(Win32Helper.WNDCLASSEX)),
                 style = 0x0020, // CS_OWNDC
                 lpfnWndProc = WndProc,
                 hInstance = Marshal.GetHINSTANCE(typeof(Program).Module),
                 lpszClassName = className
             };
 
-            RegisterClassEx(ref wndClass);
+            Win32Helper.RegisterClassEx(ref wndClass);
 
-            _width = GetSystemMetrics(0);  // SM_CXSCREEN
-            _height = GetSystemMetrics(1); // SM_CYSCREEN
+            WindowWidth = Win32Helper.GetSystemMetrics(0);  // SM_CXSCREEN
+            WindowHeight = Win32Helper.GetSystemMetrics(1); // SM_CYSCREEN
 
             // Create a borderless popup window with the layered style.
-            hWnd = CreateWindowEx(
-                WS_EX_LAYERED,
+            var hWnd = Win32Helper.CreateWindowEx(
+                (int)Win32Helper.WindowStyles.WS_EX_LAYERED,
                 className,
                 WindowTitle,
-                WS_POPUP,
+                (int)Win32Helper.WindowStyles.WS_POPUP,
                 0, 0,
-                _width, _height,
+                WindowWidth, WindowHeight,
                 IntPtr.Zero, IntPtr.Zero, wndClass.hInstance, IntPtr.Zero);
 
-            SetAlwaysOnTop(hWnd, true);
+            return hWnd;
         }
 
+        private void SetAlwaysOnTop()
+        {
+            IntPtr topmostFlag = (IntPtr)Win32Helper.HWND_TOPMOST;
+            Win32Helper.SetWindowPos(hWnd, topmostFlag, 0, 0, 0, 0,
+                (int)Win32Helper.SetWindowPosFlags.SWP_NOSIZE |
+                (int)Win32Helper.SetWindowPosFlags.SWP_NOMOVE |
+                (int)Win32Helper.SetWindowPosFlags.SWP_SHOWWINDOW);
+        }
+
+        private void RemoveTaskbarIcon()
+        {
+            int exStyle = Win32Helper.GetWindowLong(hWnd, (int)Win32Helper.WindowLongs.GWL_EXSTYLE);
+            Win32Helper.SetWindowLong(hWnd, (int)Win32Helper.WindowLongs.GWL_EXSTYLE, exStyle | (int)Win32Helper.WindowStyles.WS_EX_TOOLWINDOW);
+        }
+
+        // Window Procedure
+        private static IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (msg)
+            {
+                case (int)Win32Helper.WindowMessages.WM_PAINT:
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_SIZE:
+                    Console.WriteLine($"Window resized: {wParam} {lParam}");
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_KEYDOWN:
+                    Console.WriteLine($"Key Pressed: {wParam}");
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_MOUSEMOVE:
+                    int x = lParam.ToInt32() & 0xFFFF;
+                    int y = (lParam.ToInt32() >> 16) & 0xFFFF;
+                    Console.WriteLine($"Mouse moved: X={x}, Y={y}");
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_LBUTTONDOWN:
+                    Console.WriteLine("Left mouse button clicked.");
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_DROPFILES:
+                    Console.WriteLine("File dropped!");
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_SETCURSOR:
+                    Win32Helper.SetCursor(Win32Helper.LoadCursor(IntPtr.Zero, Win32Helper.IDC_ARROW));
+                    return IntPtr.Zero;
+
+                case (int)Win32Helper.WindowMessages.WM_DESTROY:
+                    Win32Helper.PostQuitMessage(0);
+                    return IntPtr.Zero;
+            }
+            return Win32Helper.DefWindowProcW(hWnd, msg, wParam, lParam);
+        }
     }
 }
